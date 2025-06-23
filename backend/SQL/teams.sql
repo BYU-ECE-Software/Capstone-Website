96ad3670d059034a36da008e20b7b9338fe349f7
masonjlewis <mage.j.lewis@gmail.com>
2025-06-23 08:38:02 -0600

    Create teams table and add test data (#2)

--- backend/SQL/teams.sql
+++ backend/SQL/teams.sql
@@ -1,1 +1,19 @@
---TODO
+USE capstone;
+
+CREATE TABLE IF NOT EXISTS teams (
+    team_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    school_year nvarchar(9) NOT NULL, -- 9 chars, eg. "2025-2026"
+    coach_id INT NOT NULL,
+    -- students will be connected to a team on their own table. See users.team_id
+    grading_coach_1_id INT, -- required?
+    grading_coach_2_id INT,
+    er_director_id INT, -- required?
+    long_distance_access_code INT,
+    caedm_group_folder INT,
+    project_id INT NOT NULL,
+    logo nvarchar(255), -- file path
+    team_name nvarchar(255) NOT NULL,
+    email_list longtext, -- json array?
+    team_box_folder INT,
+    class_doc_folder INT
+);
--- backend/daos/testData.js
+++ backend/daos/testData.js
@@ -34,6 +34,36 @@ const users = [
     }
 ];
 
+const teams = [
+    {
+        school_year: '2025-2026',
+        coach_id: 2, // brother seinfeld
+        long_distance_access_code: 1, //?
+        caedm_group_folder: 27,
+        project_id: 1,
+        logo: "./assets/team_1.jpg",
+        team_name: "The Best in the World",
+        email_list: "['test@email.com', 'test2@email.com']",
+        team_box_folder: 5290,
+        class_doc_folder: 6248
+    }
+];
+// team_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//     school_year nvarchar(9) NOT NULL, -- 9 chars, eg. "2025-2026"
+//     coach_id INT NOT NULL,
+//     -- students will be connected to a team on their own table. See users.team_id
+//     grading_coach_1_id INT, -- required?
+//     grading_coach_2_id INT,
+//     er_director_id INT, -- required?
+//     long_distance_access_code INT,
+//     caedm_group_folder INT,
+//     project_id INT NOT NULL,
+//     logo nvarchar(255), -- file path
+//     team_name nvarchar(255) NOT NULL,
+//     email_list longtext, -- json array?
+//     team_box_folder INT,
+//     class_doc_folder INT
+
 const connection = require('../db/connection');
 
 function insertLine(tableName, data) {
@@ -59,7 +89,10 @@ function insertLine(tableName, data) {
 
 function createTestData() {
     for (let i = 0; i < users.length; i++) {
-        insertLine("users", users[i]);
+        //insertLine("users", users[i]);
+    }
+    for (let i = 0; i < teams.length; i++) {
+        //insertLine("teams", teams[i]);
     }
 }
 

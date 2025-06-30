16e073a84c3b56bd800cf1180a6396d08facc135
masonjlewis <mage.j.lewis@gmail.com>
2025-06-30 13:19:44 -0600

    Move the endpoint calls to a single file (#10)

--- frontend/src/api/endpointCalls.js
+++ frontend/src/api/endpointCalls.js
@@ -1,3 +1,15 @@
 /**
  * Export functions that do the useEffect.fetch stuff so that we don't have to do it in every component that accesses the server
  */
+
+exports.fetchTeamById = async (id) => {
+    const response = await fetch(`/teams/${id}`);
+    if (!response.ok) throw new Error('Failed to fetch team');
+    return await response.json();
+}
+
+exports.fetchTeamIds = async () => {
+    const response = await fetch(`/teams`);
+    if (!response.ok) throw new Error('Failed to fetch teams');
+    return await response.json();
+}

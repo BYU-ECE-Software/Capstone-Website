08de25de5d6404a3af00b967970d64154c65e0fe
magejets <mage.j.lewis@gmail.com>
2025-07-10 17:55:44 -0600

    Create team_coaches table (#14)

--- backend/SQL/roles.sql
+++ backend/SQL/roles.sql
@@ -6,5 +6,7 @@ CREATE TABLE IF NOT EXISTS roles (
     -- figure out how to link this to permissions. Talk to Roman
 );
 
-INSERT INTO roles (role_name) -- before using this add the other columns that we deem necessary
-VALUES ('Admin'), ('Student'), ('Coach')
+-- INSERT INTO roles (role_name) -- before using this add the other columns that we deem necessary
+-- VALUES ('Admin'), ('Student'), ('Coach')
+
+-- Right now on the backend we are treating role_id = 1 as student and 2 as coach. This should be easy to change once roles actually come into play
--- backend/SQL/users.sql
+++ backend/SQL/users.sql
@@ -9,11 +9,19 @@ CREATE TABLE IF NOT EXISTS users (
     net_id VARCHAR(63) NOT NULL, -- shouldn't be longer than that
     byu_id INT NOT NULL, -- should it be an nvarchar(9) since it needs to be a 9 digit number?
     email VARCHAR(255) NOT NULL,
-    team_id INT, -- will reference the team table. Does this need to be a string instead? How will this need to be different for coaches?
-    role_id INT NOT NULL, -- roles table will be very important. Tried to name it just role, but that is already a reserved keyword. 1 = Admin, 2 = Student, 3 = Coach
+    team_id INT, -- How will this need to be different for coaches?
+    role_id INT NOT NULL, -- roles table will be very important. Tried to name it just role, but that is already a reserved keyword.
     phone VARCHAR(12), -- 7 for number, 3 for area code, 2 for country code
-    photo MEDIUMTEXT -- file path to the assets folder ./assets/
+    photo MEDIUMTEXT, -- file path to the assets folder ./assets/
+    FOREIGN KEY (team_id) REFERENCES teams(team_id),
+    FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
 
 
 -- WRITE AN INSERT STATEMENT ONCE WE GET THE SAML stuff worked out
+
+
+-- So here's the thing. Students will have a team_id, but coaches won't. 
+-- Should we have seperate tables for coach and student, or just assume that the coach team_id will be null?
+-- I'm leaning toward the latter, just for simplicity's sake. The backend just needs to know that.
+-- Maybe once we transition to prisma this'll be a non-issue.

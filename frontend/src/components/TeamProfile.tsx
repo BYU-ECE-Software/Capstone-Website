090aa838af6ad11dff4edf814e282482b6f5aa43
magejets <mage.j.lewis@gmail.com>
2025-07-17 15:58:13 -0600

    Add links to team pages and make team numbers clickable in directory (#29)

--- frontend/src/components/TeamProfile.tsx
+++ frontend/src/components/TeamProfile.tsx
@@ -3,6 +3,7 @@ import UserTeamDirectoryLine from './users/UserTeamDirectoryLine';
 import { fetchTeamById } from '../api/endpointCalls';
 import './TeamProfile.css';
 import { Team } from '../types/team';
+import { Link } from 'react-router-dom';
 
 export default function TeamProfile({ team_id=0 }: {team_id?: number}) {
     const [team, setTeam] = useState<Team | null>(null);
@@ -28,7 +29,7 @@ export default function TeamProfile({ team_id=0 }: {team_id?: number}) {
     // maybe I'll want to move this code to a team component and then just call it from here with the url param, and from teamsdirectory and input the team_id of each array item
     return (
         <div>
-            <h1 className='"text-4xl text-byuNavy font-semibold mb-4'>Team {final_id}</h1>
+            <h1 className='"text-4xl text-byuNavy font-semibold mb-4'><Link to={`/teams/${team.team.team_id}`}>Team {team.team.team_number}</Link></h1>
             <img alt={`${final_id}`} src={`http://localhost:3001/assets/${team.team.logo}`} className='team-logo'/>
             {team.coach.length > 0 ?
                 (team.coach.map((coach) => (<div key={coach.user_id} id="coach-info">
